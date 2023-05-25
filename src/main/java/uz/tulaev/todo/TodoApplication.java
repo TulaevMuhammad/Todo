85da048f8ef43ecb1d324eee4db23408d199c44f
Tulaev Muhammad <tulaevmuhammadman@gmail.com>
2023-05-26 03:27:12 +0500

    32

--- src/main/java/uz/tulaev/todo/TodoApplication.java
+++ src/main/java/uz/tulaev/todo/TodoApplication.java
@@ -26,30 +26,30 @@ public class TodoApplication {
         SpringApplication.run(TodoApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner commandLineRunner() {
-//        return args -> {
-//            User user = new User();
-//            user.setUsername("JohnDoe");
-//            user.setPassword("password123");
-//            userRepository.save(user);
-//
-//            Task task1 = new Task();
-//            task1.setTitle("Task 1");
-//            task1.setDescription("Description for Task 1");
-//            task1.setDueDate(LocalDate.now().plusDays(3));
-//            task1.setCompleted(false);
-//            task1.setUser(user);
-//            taskRepository.save(task1);
-//
-//            Task task2 = new Task();
-//            task2.setTitle("Task 2");
-//            task2.setDescription("Description for Task 2");
-//            task2.setDueDate(LocalDate.now().plusDays(5));
-//            task2.setCompleted(false);
-//            task2.setUser(user);
-//            taskRepository.save(task2);
-//        };
-//    }
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            User user = new User();
+            user.setUsername("JohnDoe");
+            user.setPassword("password123");
+            userRepository.save(user);
+
+            Task task1 = new Task();
+            task1.setTitle("Task 1");
+            task1.setDescription("Description for Task 1");
+            task1.setDueDate(LocalDate.now().plusDays(3));
+            task1.setCompleted(false);
+            task1.setUser(user);
+            taskRepository.save(task1);
+
+            Task task2 = new Task();
+            task2.setTitle("Task 2");
+            task2.setDescription("Description for Task 2");
+            task2.setDueDate(LocalDate.now().plusDays(5));
+            task2.setCompleted(false);
+            task2.setUser(user);
+            taskRepository.save(task2);
+        };
+    }
 
 }
